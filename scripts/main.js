a8f1bbaa85610a740f8e1d438ea3cc85afab77f6
Kirill <efremenkov1999@gmail.com>
2020-09-29 22:22:42 +0300

    add generate some sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
+'use strict';
 const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
-const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
 const subCatalog = document.querySelector('.subcatalog');
 const subCatalogHeader = document.querySelector('.subcatalog-header');
 const btnReturn = document.querySelector('.btn-return');
+
+const overlay = document.createElement('div');
+
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
 
 const openMenu = () => {
     catalog.classList.add('open');
